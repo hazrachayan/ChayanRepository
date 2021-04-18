61863ba13ba46dc94cf1b4e8ce6ec1abfc6972b5
hazrachayan <chayan.hazra@datarobot.com>
2021-04-18 16:08:31 +0530

    default folder output

--- K8sUsingFabric/src/main/java/com/paxata/engine/Supportability.java
+++ K8sUsingFabric/src/main/java/com/paxata/engine/Supportability.java
@@ -26,6 +26,7 @@ public class Supportability {
     public static final String PIPELINE_PROXY_POD_NAME = "pax-installation-pipeline-proxy";
     public static final String PIPELINE_PROXY_CONTAINER = "pipeline-proxy";
     public static final int LINES_OF_LOGS = 1000;
+    public static final String OUTPUT_DIRECTORY = "./target/output/";
 
     KubernetesClient client = null;
 
@@ -61,16 +62,16 @@ public class Supportability {
                 for(int i=0; i<containers.size();i++){
                     if (containers.get(i).getName().equalsIgnoreCase(PAX_CONTAINER)){
                         readLog(nameSpace, pod.getMetadata().getName(), containers.get(i).getName(),
-                                LINES_OF_LOGS, "/Users/chayan.hazra/Downloads/frontend.log");
+                                LINES_OF_LOGS, new File(OUTPUT_DIRECTORY + File.pathSeparator + "paxserver/logs/frontend.log"));
                         System.out.println("-> -> Containers: " + containers.get(i).getName());
 
                         downloadFileFromContainer(nameSpace, pod.getMetadata().getName(),
                                 containers.get(i).getName(),"/usr/local/paxata/server/config/px.properties",
-                                new File("/Users/chayan.hazra/Downloads/px.properties"));
+                                new File(OUTPUT_DIRECTORY + File.pathSeparator + "paxserver/config/px.properties"));
 
                         downloadFileFromContainer(nameSpace, pod.getMetadata().getName(),
                                 containers.get(i).getName(),"/usr/local/paxata/server/config/px-default.properties",
-                                new File("/Users/chayan.hazra/Downloads/px-default.properties"));
+                                new File(OUTPUT_DIRECTORY + File.pathSeparator + "paxserver/config/px-default.properties"));
                     }
 
                 }
@@ -82,7 +83,7 @@ public class Supportability {
                 for(int i=0; i<containers.size();i++){
                     if (containers.get(i).getName().equalsIgnoreCase(MONGO_CONTAINER)){
                         readLog(nameSpace, pod.getMetadata().getName(), containers.get(i).getName(),
-                                LINES_OF_LOGS, "/Users/chayan.hazra/Downloads/mongo.log");
+                                LINES_OF_LOGS, new File(OUTPUT_DIRECTORY + File.pathSeparator + "mongo/logs/mongo.log"));
                         System.out.println("-> -> Containers: " + containers.get(i).getName());
                     }
 
@@ -96,12 +97,13 @@ public class Supportability {
                 for(int i=0; i<containers.size();i++){
                     if (containers.get(i).getName().equalsIgnoreCase(PIPELINE_MASTER_CONTAINER)){
                         readLog(nameSpace, pod.getMetadata().getName(), containers.get(i).getName(),
-                                LINES_OF_LOGS, "/Users/chayan.hazra/Downloads/master.log");
+                                LINES_OF_LOGS, new File(OUTPUT_DIRECTORY + File.pathSeparator + "pipeline/logs/master.log"));
                         System.out.println("-> -> Containers: " + containers.get(i).getName());
                     }
                     if (containers.get(i).getName().equalsIgnoreCase(PIPELINE_WORKER_CONTAINER)){
                         readLog(nameSpace, pod.getMetadata().getName(), containers.get(i).getName(),
-                                LINES_OF_LOGS, "/Users/chayan.hazra/Downloads/" + workerCounter + "-worker.log");
+                                LINES_OF_LOGS, new File(OUTPUT_DIRECTORY
+                                        + File.pathSeparator + "pipeline/logs/" + workerCounter + "-worker.log"));
                         System.out.println("-> -> Containers: " + containers.get(i).getName() + "\n" + workerCounter + "-worker.log");
                         workerCounter++;
                     }
@@ -116,7 +118,7 @@ public class Supportability {
                 for(int i=0; i<containers.size();i++){
                     if (containers.get(i).getName().equalsIgnoreCase(PIPELINE_PROXY_CONTAINER)){
                         readLog(nameSpace, pod.getMetadata().getName(), containers.get(i).getName(),
-                                LINES_OF_LOGS, "/Users/chayan.hazra/Downloads/pipeline-proxy.log");
+                                LINES_OF_LOGS, new File(OUTPUT_DIRECTORY + File.pathSeparator + "pipeline-proxy/logs/pipeline-proxy.log"));
                         System.out.println("-> -> Containers: " + containers.get(i).getName());
                     }
 
@@ -126,30 +128,38 @@ public class Supportability {
     }
 
     public void downloadFileFromContainer(String namespace, String podname, String container, String filePathInContainer, File downloadLocation){
+        createFolder(downloadLocation);
         client.pods()
                 .inNamespace(namespace)
                 .withName(podname)
                 .inContainer(container)
                 .file(filePathInContainer)
                 .copy(downloadLocation.toPath());
-        System.out.println("Downloaded successfully!");
     }
 
-    public void readLog(String namespace, String podname, String container, int lineCount, String filePath){//https://www.programcreek.com/java-api-examples/index.php?api=io.fabric8.kubernetes.client.dsl.LogWatch
+    public void readLog(String namespace, String podname, String container, int lineCount, File filePath){//https://www.programcreek.com/java-api-examples/index.php?api=io.fabric8.kubernetes.client.dsl.LogWatch
         try{
             PrettyLoggable<String, LogWatch> tailingLines = client.pods().inNamespace(namespace)
                     .withName(podname)
                     .inContainer(container)
                     .tailingLines(lineCount);
             String log = tailingLines.getLog();
+            createFolder(filePath);
             if (!log.isEmpty()) {
-                FileWriter fileWriter = new FileWriter(new File(filePath));
+                FileWriter fileWriter = new FileWriter(new File(filePath.getAbsolutePath()));
                 fileWriter.write(log);
                 fileWriter.close();
             }
-            System.out.println("logs written to file");
         }catch (Exception e){
             e.printStackTrace();
+        }
+    }
+
+    public void createFolder(File filename){
+        if(!filename.exists())
+        {
+            String directory = filename.getParent();
+            new File(directory).mkdirs();
         }
     }
 
